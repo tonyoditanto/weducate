6dfd324e1f0a6dda486d0037597332174818daaf
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2020-03-11 13:46:18 +0700

    Initial Commit

---
+++ Weducate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weducate
+//
+//  Created by Arifin Firdaus on 11/03/20.
+//  Copyright © 2020 Weducate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
