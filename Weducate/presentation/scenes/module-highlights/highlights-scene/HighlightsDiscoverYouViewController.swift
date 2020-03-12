e939975e9bb9bf50ecc54d8954999695cf2797ce
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2020-03-12 15:10:54 +0700

    Create HighlightsDiscoverYouViewController

---
+++ Weducate/presentation/scenes/module-highlights/highlights-scene/HighlightsDiscoverYouViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HighlightsDiscoverYouViewController.swift
+//  Weducate
+//
+//  Created by Arifin Firdaus on 12/03/20.
+//  Copyright © 2020 Weducate. All rights reserved.
+//
+
+import UIKit
+
+class HighlightsDiscoverYouViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
