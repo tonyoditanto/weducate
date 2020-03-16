5533ea3331f03740a3e5d3f6c52759d2b44dbbc5
Bagus setiawan <setiawanbagus4315@gmail.com>
2020-03-16 22:42:29 +0700

    progress multiple questions

--- Weducate/presentation/scenes/module-assesment/Assesment-scene/AssesmentViewController.swift
+++ Weducate/presentation/scenes/module-assesment/Assesment-scene/AssesmentViewController.swift
@@ -2,33 +2,19 @@
 //  AssesmentViewController.swift
 //  Weducate
 //
-//  Created by Bagus setiawan on 12/03/20.
+//  Created by Bagus setiawan on 16/03/20.
 //  Copyright © 2020 Weducate. All rights reserved.
 //
 
 import UIKit
 
-class AllertViewController: UIViewController {
+class AssesmentViewController: UIViewController {
 
-@IBOutlet weak var logoView : UIImageView!
+    @IBOutlet weak var imageAssesmentView: UIImageView!
     
-var logo : UIImage?
-
-override func viewDidLoad() {
-    super.viewDidLoad()
-    logoView.image = logo
+    @IBOutlet weak var questionView: UITextField!
     
-}
-    
-func UIAllertController() {
-    let alertController = UIAlertController(title: "Question ?", message: "Are you ready to find out about your interest ?", preferredStyle: UIAlertController.Style.alert)
-
-    alertController.addAction(UIAlertAction(title: "NO", style : UIAlertAction.Style.default, handler: { _ in
-        //Cancel Action
-    }))
-    alertController.addAction(UIAlertAction(title: "Yes", style: UIAlertAction.Style.default, handler: {(_: UIAlertAction!) in
-                                    //Sign out action
-    }))
-    self.present(alertController, animated: true, completion: nil)
-}
+    override func viewDidLoad() {
+       
+    }
 }
