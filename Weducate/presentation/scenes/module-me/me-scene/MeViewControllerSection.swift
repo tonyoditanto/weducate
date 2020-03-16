452f3196b882cd853d6d36e9d6018767a35ac887
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2020-03-16 10:33:28 +0700

    Added alert when sign out

---
+++ Weducate/presentation/scenes/module-me/me-scene/MeViewControllerSection.swift
@@ -0,0 +1,15 @@
+//
+//  MeViewControllerSection.swift
+//  Weducate
+//
+//  Created by Arifin Firdaus on 16/03/20.
+//  Copyright © 2020 Weducate. All rights reserved.
+//
+
+import Foundation
+
+struct MeViewControllerSection {
+    static let SECTION_PROFILE_DETAIL = 0
+    static let SECTION_MY_WEDUCATE_JOURNEY = 1
+    static let SECTION_SIGN_OUT = 2
+}
